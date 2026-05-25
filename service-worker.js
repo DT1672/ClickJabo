ff149147a958919c049e1091bd7b0468dcadc1bd
DT1672 <dong.kim85@gmail.com>
2026-05-25 11:23:10 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,39 +1,117 @@
-const CACHE_NAME = "clickjabo-v2";
+const CACHE_NAME = "clickjabo-v3";
 
 const urlsToCache = [
+
   "/",
   "/index.html",
   "/style.css",
   "/app.js",
   "/routes.json",
   "/icon.png",
-  "/manifest.json"
+  "/manifest.json",
+  "/banner.png",
+  "/splash.png",
+
+  "https://cdn.jsdelivr.net/npm/choices.js/public/assets/styles/choices.min.css",
+
+  "https://cdn.jsdelivr.net/npm/choices.js/public/assets/scripts/choices.min.js"
+
 ];
 
-self.addEventListener("install", event => {
+/* INSTALL */
 
-  event.waitUntil(
+self.addEventListener(
 
-    caches.open(CACHE_NAME)
+  "install",
+
+  event => {
+
+    event.waitUntil(
+
+      caches.open(
+        CACHE_NAME
+      )
+
       .then(cache => {
-        return cache.addAll(urlsToCache);
+
+        return cache.addAll(
+          urlsToCache
+        );
+
       })
 
-  );
+    );
 
-});
+  }
 
-self.addEventListener("fetch", event => {
+);
 
-  event.respondWith(
+/* FETCH */
 
-    caches.match(event.request)
+self.addEventListener(
+
+  "fetch",
+
+  event => {
+
+    event.respondWith(
+
+      caches.match(
+        event.request
+      )
+
       .then(response => {
 
-        return response || fetch(event.request);
+        return response ||
+
+        fetch(
+          event.request
+        );
 
       })
 
-  );
+    );
 
-});
+  }
+
+);
+
+/* ACTIVATE */
+
+self.addEventListener(
+
+  "activate",
+
+  event => {
+
+    event.waitUntil(
+
+      caches.keys()
+
+      .then(keys => {
+
+        return Promise.all(
+
+          keys.map(key => {
+
+            if(
+              key !== CACHE_NAME
+            ){
+
+              return caches.delete(
+                key
+              );
+
+            }
+
+          })
+
+        );
+
+      })
+
+    );
+
+  }
+
+);
